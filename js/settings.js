d8a174db618d3b4b7c48cc7d88b9e9629fa50a9c
38136 <38136.hexaware@gmail.com>
2017-09-27 05:37:40 +0530

    Add all Files

---
+++ js/settings.js
@@ -0,0 +1,24 @@
+'use strict';
+
+/* -------------------------------------------------------------------
+Copyright (c) 2017-2017 Hexaware Technologies
+This file is part of the Innovation LAB - Offline Bot.
+------------------------------------------------------------------- */
+
+
+define({
+
+    // accessToken: "e58329c450cb4896a4ca17454ac06737",
+    accessToken: "ede17230c7954ddca7fde1b5dc85d37f",
+    chatServerURL : "https://api.api.ai/v1/",
+
+    applicationTitle: "VFS Global",
+    userTitle : "Me",
+    botTitle : "VFS Assistant",
+    botAvatar : "avatar/vfslogo.png",
+    userAvatar : "avatar/user.png",
+    attachmentUploadURL : "",
+    platform : "facebook",
+
+    showHeader: true
+});
